48e651ea776b14186ab12d9cbfc230ff0646dc73
Tomas Grubliauskas <v3nomzxgt8@gmail.com>
2019-11-15 23:42:33 +0100

    install npm packages on first run

--- init.ps1
+++ init.ps1
@@ -1,7 +1,13 @@
 $Env:WEB = $PSScriptRoot + "\web"
+$buildFolder = $PSScriptRoot + ".\build"
 
 # Import the module with "generate" functions
-gci -Recurse .\build -Filter *.psm1 | % {
+gci -Recurse $buildFolder -Filter *.psm1 | % {
     Remove-Module ([io.path]::GetFileNameWithoutExtension($_)) -ErrorAction Ignore
     Import-Module (join-path $_.DirectoryName $_.PSChildName) -DisableNameChecking
+}
+
+# Install npm packages if first run
+if (-not (Test-Path "$Env:WEB\node_modules")) {
+    Install-Dependencies
 }
